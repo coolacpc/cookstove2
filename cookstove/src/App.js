3b9c4d616f45145e2dad987f97b53ef3a66c6cbc
coolacpc <coolacpc@gmail.com>
2023-06-18 05:46:00 +0300

    fixing routing issues with clients page

--- cookstove/src/App.js
+++ cookstove/src/App.js
@@ -10,7 +10,7 @@ function App() {
 				<div>
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/clients" element={<Clients />} />
+						<Route path="clients" element={<Clients />} />
 					</Routes>
 				</div>
 			</BrowserRouter>
