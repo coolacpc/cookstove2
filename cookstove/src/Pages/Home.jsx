24b9f478c5bbf84e41d703f707d54103e5307fba
coolacpc <coolacpc@gmail.com>
2023-06-18 16:23:28 +0300

    added about us component

--- cookstove/src/Pages/Home.jsx
+++ cookstove/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
+import About from "../components/About";
 import HomePricing from "../components/HomePricing";
 import Feature from "../components/Feature";
 import Footer from "../components/Footer";
@@ -8,6 +9,7 @@ function Home() {
 	return (
 		<div>
 			<Hero />
+			<About />
 			<Feature />
 			<HomePricing />
 			<Footer />
