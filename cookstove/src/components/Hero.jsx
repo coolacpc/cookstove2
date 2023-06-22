9b74629c74faead5f99b02a8bddea2c13c46a720
coolacpc <coolacpc@gmail.com>
2023-06-22 18:08:08 +0300

    added small heading on hero

--- cookstove/src/components/Hero.jsx
+++ cookstove/src/components/Hero.jsx
@@ -108,6 +108,9 @@ export default function Hero() {
 						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl">
 							Stove usage data and insights.
 						</h1>
+						<p className="mt-3 text-lg leading-8 text-gray-600">
+							Catalysing Investment in Clean-Cooking
+						</p>
 						<p className="mt-6 text-lg leading-8 text-gray-600">
 							Stove Data Works (SDW) is a cutting-edge Internet of
 							Things (IoT)-based solution designed to
@@ -125,7 +128,7 @@ export default function Hero() {
 								href="#"
 								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 							>
-								Order Now!
+								Pre-Book
 							</a>
 							<a
 								href="#"
